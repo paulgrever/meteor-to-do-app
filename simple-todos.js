a7de1090505fbf32cf23fb4a9549ba89a11292e7
Paul Grever <paulgrever@gmail.com>
2015-08-03 23:26:48 -0600

    finished section 4

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,19 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+  Template.body.events({
+    "submit .new-task": function(event) {
+      event.preventDefault();
+
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      event.target.text.value = "";
     }
   });
 }
